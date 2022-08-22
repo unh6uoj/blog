67886bca975e216c8bb91f963c3602ddeb3fe2c3
unh6uoj <fpdlxms159@gmail.com>
2022-08-22 18:26:33 +0900

    update : meta-config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -6,11 +6,11 @@ module.exports = {
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: `https://github.com/unh6uoj`, // Your GitHub account
+    github: `unh6uoj`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: `https://www.facebook.com/profile.php?id=100003946489329`, // Your Facebook account
-    linkedin: `https://www.linkedin.com/in/종윤-정-9369a1208/`, // Your LinkedIn account
-    instagram: `https://instagram.com/ung6uoj`, // Your Instagram account
+    facebook: `100003946489329`, // Your Facebook account
+    linkedin: `종윤-정-9369a1208/`, // Your LinkedIn account
+    instagram: `ung6uoj`, // Your Instagram account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
