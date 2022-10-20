6aa28ea91bac678c65014cb7e5dd6f6a2551c7b0
unh6uoj <fpdlxms159@gmail.com>
2022-10-20 18:05:48 +0900

    add : css

---
+++ src/pages/css.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef, useState } from 'react'
+import './css.scss';
+
+function Hanna() {
+    return (
+        <div id='main'>
+            <h1>
+                CSS공부 했어?
+            </h1>
+        </div>
+    )
+}
+
+export default Hanna;
